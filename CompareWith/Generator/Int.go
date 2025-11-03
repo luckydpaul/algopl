59f6a50d7b4d8f7f14ae082a80ec0a2f8b847a43
zh <zhcrmy@163.com>
2025-11-03 14:21:16 +0800

    into

--- CompareWith/Generator/Int.go
+++ CompareWith/Generator/Int.go
@@ -1,14 +1,35 @@
 package Generator
 
 import (
+	"math/rand"
 	"reflect"
+	"sync"
+	"time"
 )
 
-type IntGenerator struct {
+// 这个东西是我们在构建 intgenerator 时必要传进来的东西，设置的是需要生成的字符大小，等等
+type IntConfig struct {
+	Min *int //最小值
+	Max *int //最大值
+
+	Parity *string
+
+	DIYS []func(int) bool
 }
 
-func NewIntGenerator() *IntGenerator {
-	return &IntGenerator{}
+type IntGenerator struct {
+	mu    sync.Mutex //保证并发安全
+	r     *rand.Rand
+	cfg   IntConfig //配置信息
+	descs []struct {
+		Even   *bool //奇偶
+		MinMax *[2]int
+	}
+}
+
+func NewIntGenerator(cfg IntConfig) *IntGenerator {
+	g := &IntGenerator{}
+	g.r = rand.New(rand.NewSource(time.Now().UnixNano()))
 }
 
 func (i *IntGenerator) Exec() (interface{}, error) {
--- CompareWith/Generator/type.go
+++ CompareWith/Generator/type.go
@@ -6,3 +6,8 @@ type Generator interface {
 	Exec() (interface{}, error)
 	GetType() (reflect.Type, error)
 }
+
+const (
+	Generator_sturct_odd  = "odd"  //奇数
+	Generator_sturct_even = "even" //偶数
+)
